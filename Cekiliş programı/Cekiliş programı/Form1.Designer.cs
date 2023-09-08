50e2925d9f35e620569082602b20fc259827ca62
Mehmet Batuhan Demir <53302194+ottoman81@users.noreply.github.com>
2023-09-08 20:35:17 +0300

    Add files via upload

---
+++ Cekiliş programı/Cekiliş programı/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace Cekiliş_programı
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lstcekilen = new System.Windows.Forms.ListBox();
+            this.lstKatilimcilar = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Çekilecekler";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(343, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Çekilenler";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(185, 308);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Null";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(185, 265);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "İlerleme Sayısı";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(185, 152);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Rastgele çekilen sayısı";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(185, 187);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Null";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 17);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Item ismi";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 152);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(167, 290);
+            this.listBox1.TabIndex = 7;
+            // 
+            // lstcekilen
+            // 
+            this.lstcekilen.FormattingEnabled = true;
+            this.lstcekilen.Location = new System.Drawing.Point(346, 152);
+            this.lstcekilen.Name = "lstcekilen";
+            this.lstcekilen.Size = new System.Drawing.Size(182, 290);
+            this.lstcekilen.TabIndex = 8;
+            // 
+            // lstKatilimcilar
+            // 
+            this.lstKatilimcilar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lstKatilimcilar.FormattingEnabled = true;
+            this.lstKatilimcilar.ItemHeight = 25;
+            this.lstKatilimcilar.Location = new System.Drawing.Point(197, 17);
+            this.lstKatilimcilar.Name = "lstKatilimcilar";
+            this.lstKatilimcilar.Size = new System.Drawing.Size(336, 29);
+            this.lstKatilimcilar.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(303, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 40);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Çekilişi Başlat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(185, 356);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 29);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Listeye dosyadan veri aktar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(184, 407);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(151, 35);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Listeyi Temizle";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(13, 59);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Listeye ekle";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(13, 87);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "Listeden sil";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 20);
+            this.textBox1.TabIndex = 15;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 453);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lstKatilimcilar);
+            this.Controls.Add(this.lstcekilen);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox lstcekilen;
+        private System.Windows.Forms.ListBox lstKatilimcilar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Cekiliş programı/Cekiliş programı/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Cekiliş_programı
+{
+    public partial class Form1 : Form
+    {
+        private int currentIndex;
+        private List<string> katilimcilar = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int rgl = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            timer2.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            rgl = 0;
+            currentIndex = 0;
+            Random random = new Random();
+            int sayı = random.Next(10, 210);
+            rgl = sayı;
+            label6.Text = Convert.ToString(rgl);
+
+            if (timer1.Interval >= rgl)
+            { timer1.Interval = 10; }
+            else
+                timer1.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Metin Dosyaları (*.txt)|*.txt|Tüm Dosyalar (*.*)|*.*";
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // Dosya içeriğini okuyup katilimcilar listesine ekliyoruz
+                    katilimcilar.Clear();
+                    using (StreamReader sr = new StreamReader(openFileDialog.FileName))
+                    {
+                        string satir;
+                        while ((satir = sr.ReadLine()) != null)
+                        {
+                            katilimcilar.Add(satir);
+
+                        }
+                    }
+
+                    // ListBox'a aktarıyoruz
+                    lstKatilimcilar.Items.Clear();
+                    listBox1.Items.Clear();
+                    foreach (string katilimci in katilimcilar)
+                    {
+                        lstKatilimcilar.Items.Add(katilimci);
+                        listBox1.Items.Add(katilimci);
+                    }
+
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Hata: " + ex.Message);
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            lstKatilimcilar.Items.Clear();
+            listBox1.Items.Clear();
+            button1.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                timer1.Interval += 1;
+                currentIndex = (currentIndex + 1) % lstKatilimcilar.Items.Count;
+                lstKatilimcilar.SelectedIndex = currentIndex;
+                label3.Text = timer1.Interval.ToString();
+            }
+            catch { }
+            if (timer1.Interval >= rgl)
+            {
+                timer1.Enabled = false;
+                if (katilimcilar.Count > 0)
+                {
+                    if (timer1.Enabled == false)
+                    {
+
+                        int kazananIndex = lstKatilimcilar.SelectedIndex;
+                        string kazananKisi = katilimcilar[kazananIndex];
+
+
+
+                        // Çekiliş sonucunu ekrana yazdırma
+                        MessageBox.Show($"Kazanılan : {kazananKisi}");
+
+
+
+                        // Kazanan kişiyi listeden çıkarma
+                        lstcekilen.Items.Add(kazananKisi);
+
+
+                        katilimcilar.RemoveAt(kazananIndex);
+
+
+                        // Listeyi güncelleyerek tekrar ListBox'a yazdırma
+                        lstKatilimcilar.Items.Clear();
+                        foreach (string katilimci in katilimcilar)
+                        {
+                            lstKatilimcilar.Items.Add(katilimci);
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Tüm katılımcılar çekiliş yapıldı!");
+                }
+                timer1.Interval = 10;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (lstKatilimcilar.SelectedIndex != -1)
+            {
+                lstKatilimcilar.Items.RemoveAt(lstKatilimcilar.SelectedIndex);
+
+            }
+            else
+            {
+                MessageBox.Show("Lütfen bir öğe seçin.");
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            lstKatilimcilar.Items.Add(textBox1.Text);
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (lstKatilimcilar.Items.Count < 0)
+            {
+                button1.Enabled = false;
+                button5.Enabled = false;
+
+            }
+            else if (lstKatilimcilar.Items.Count > 0)
+            {
+                button1.Enabled = true;
+                button5.Enabled = true;
+            }
+        }
+    }
+    }
+
